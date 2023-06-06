b2115937d9a5060e695d033d646892fc49147fbd
DanieleGioia <danielegioia@outlook.com>
2023-06-06 12:13:15 +0200

    init updated with version and authors

--- scenarioReducer/__init__.py
+++ scenarioReducer/__init__.py
@@ -1,7 +1,14 @@
 from .fast_forward import Fast_forward
 from .scenario_reducer import Scenario_reducer
 
+__version__ = "0.1.0"
+__author__ = "Daniele Giovanni Gioia, pdb5627"
+
+
 __all__ = [
     "Fast_forward",
-    "Scenario_reducer"
+    "Scenario_reducer",
+    # Other
+    "__author__",
+    "__version__",
 ]
